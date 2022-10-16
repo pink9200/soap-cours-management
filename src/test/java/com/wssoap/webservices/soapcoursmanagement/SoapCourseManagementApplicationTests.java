d43b3168f4c454b77c974e79aab1155c6cdab8b6
Rose Faye <rosaliefaye2@gmail.com>
2022-10-16 17:23:20 +0200

    my first commit

---
+++ src/test/java/com/wssoap/webservices/soapcoursmanagement/SoapCourseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wssoap.webservices.soapcoursmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapCourseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
